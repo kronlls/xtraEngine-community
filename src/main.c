de13131da1f5a8ae2fbdf95ab4b96730dbf2b95a
Kron Game Development <155055038+kron-gdev@users.noreply.github.com>
2023-12-28 00:20:40 -0800

    Created main.c
    Unfinished

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/*
+ Main File for xtraEngine 
+ Community Source!
+
+
+ ~ Initiative Started ~
+ By Shane L.
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+
+
+
+}
