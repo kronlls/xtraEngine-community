0dd2cc68b3c32b2ffaf085fd57afaae4cd39d632
daunt3d <135356320+daunt3d@users.noreply.github.com>
2023-12-29 13:24:06 -0800

    Minor bug patch #2

--- src/status.cs
+++ src/status.cs
@@ -1,64 +1,49 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text;
-/* 
-[-] Init function script
-[-] Written 12/29/23
-[-] WARNING: DO NOT REWRITE THIS UNLESS YOU HAVE KNOWLEDGE ON THE PROGRAM. 
-*/
+using System.Threading; 
 
 namespace status
 {
-  
-class stat{
-  
-   static void SleepFunc(int mlsec){
-
-    Thread.Sleep(mlsec);
-  
-
-   }
-
-
-
-
-  
-  public static void status(string[] args){
-  
-  Console.WriteLine("if you are reading this: initialization success!");
-    //[-] Writes message confirming the initialization
-   
-    int bgTime = 1000;
-
-    SleepFunc(bgTime);
-
-    string ping = "http://tlochsta.dev";
-
-
-     using (HttpClient client = new HttpClient())
+    class stat
+    {
+        static void SleepFunc(int mlsec)
         {
-            client.Timeout = TimeSpan.FromSeconds(1); // Set the timeout to 1 second
+            Thread.Sleep(mlsec);
+        }
 
-            try
+        public static async Task status(string[] args)
+        {
+            Console.WriteLine("if you are reading this: initialization success!");
+
+            int bgTime = 1000;
+            SleepFunc(bgTime);
+
+            string ping = "http://tlochsta.dev";
+
+            using (HttpClient client = new HttpClient())
             {
-                HttpResponseMessage response = await client.GetAsync(urlToPing);
-                
-                if (response.IsSuccessStatusCode)
+                client.Timeout = TimeSpan.FromSeconds(1); 
+
+                try
                 {
-                    Console.WriteLine("Online - HTTP-Available [200 OK]");
+                    HttpResponseMessage response = await client.GetAsync(ping); 
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Online - HTTP-Available [200 OK]");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Response status: {response.StatusCode}");
+                    }
                 }
-                else
+                catch (HttpRequestException)
                 {
-                    Console.WriteLine($"Response status: {response.StatusCode}");
+                    Console.WriteLine(" ERROR!: HTTP Site Status Offline, modify the \'ping'\ string and input a valid domain.");
                 }
-            }
-            catch (HttpRequestException)
-            {
-                Console.WriteLine("Offline or timeout");
             }
         }
     }
 }
 
-}
